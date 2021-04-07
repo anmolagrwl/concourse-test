10c66a99a7021db49c6d39c2087247810365af93
Anmol Agrawal <aagrawal2@atlassian.com>
2021-04-08 00:00:02 +1000

    TST-11 fetch issue key from commit

--- task-scripts/deployment-script.sh
+++ task-scripts/deployment-script.sh
@@ -14,7 +14,7 @@ commit_name=$(git log -1 --pretty=%B)
 # echo $commit_name
 issue_key=''
 if [[ $commit_name =~ ^[a-zA-Z]+-[0-9]+ ]]; then
-        issue_key=${BASH_REMATCH[0]}
+        issue_key= echo "${BASH_REMATCH[0]}"
 fi
 echo $issue_key
 
